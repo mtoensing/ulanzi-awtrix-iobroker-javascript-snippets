7bdfbe2d6111589e9876d52834250609dd1c7bd5
Marc Toensing <marc.toensing@gmail.com>
2023-08-15 19:43:00 +0200

    Update basic-example.js

--- basic-example.js
+++ basic-example.js
@@ -1,7 +1,7 @@
 
 sendTo('awtrix-light', 'notification', {
     text: [
-        { "t": "Hello!", "c": "00FF00" }], repeat: 1, sound: 'ff_victory', duration: 5, stack: true, wakeup: true
+        { "t": "Hello World", "c": "00FF00" }], repeat: 1, icon: 12869, sound: 'ff_victory', duration: 5, stack: true, wakeup: true
 }, (res) => {
     if (res && res.error) {
         console.error(res.error);
